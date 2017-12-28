ad55f4e9122eda97651007c68751b49713608f08
babroval <maksimbabrou@gmail.com>
2017-12-28 17:12:20 +0100

    Changing equals()

--- src/babroval/testtask/entities/User.java
+++ src/babroval/testtask/entities/User.java
@@ -82,9 +82,7 @@ public class User implements Serializable {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
+		if (!(obj instanceof User))
 			return false;
 		User other = (User) obj;
 		if (email == null) {
