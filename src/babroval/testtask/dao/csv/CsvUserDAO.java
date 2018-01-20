4cfc5d9e7ad72a55d0c2249a278241d6a5a1686e
babroval <maksimbabrou@gmail.com>
2018-01-20 13:38:31 +0100

    Organize Imports

--- src/babroval/testtask/dao/csv/CsvUserDAO.java
+++ src/babroval/testtask/dao/csv/CsvUserDAO.java
@@ -1,7 +1,6 @@
 package babroval.testtask.dao.csv;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
