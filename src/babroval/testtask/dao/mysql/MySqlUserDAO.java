5c8c0bb5f1f334882218b071663efb9a2f2ade8f
babroval <maksimbabrou@gmail.com>
2018-01-20 15:15:07 +0100

    Use automatic resource management in JDBC

--- src/babroval/testtask/dao/mysql/MySqlUserDAO.java
+++ src/babroval/testtask/dao/mysql/MySqlUserDAO.java
@@ -17,15 +17,12 @@ public class MySqlUserDAO implements UserDAO {
 
 	@Override
 	public User storeUser(User user) {
-		Connection connection = null;
-		PreparedStatement statement = null;
-		ResultSet set = null;
 
-		try {
-			connection = ConnectionPool.getPool().getConnection();
+		String query = "INSERT INTO users (name, surname, login, email, tel) VALUES (?, ?, ?, ?, ?)";
 
-			statement = connection
-					.prepareStatement("INSERT INTO users (name, surname, login, email, tel) VALUES (?, ?, ?, ?, ?)");
+		try (Connection connection = ConnectionPool.getPool().getConnection();
+				PreparedStatement statement = connection.prepareStatement(query))
+		{
 			statement.setString(1, user.getName());
 			statement.setString(2, user.getSurname());
 			statement.setString(3, user.getLogin());
@@ -36,24 +33,19 @@ public class MySqlUserDAO implements UserDAO {
 
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		} finally {
-			ConnectionPool.getPool().closeDbResources(connection, statement, set);
 		}
 		return null;
 	}
 
 	@Override
 	public void updateUser(User user) {
-		Connection connection = null;
-		PreparedStatement statement = null;
-		ResultSet set = null;
 
-		try {
-			connection = ConnectionPool.getPool().getConnection();
-
-			statement = connection.prepareStatement(
-					"UPDATE USERS SET name = ?, surname = ?, login = ?, email = ?, tel = ? WHERE login='"
-							+ user.getLogin() + "'");
+		String query = "UPDATE USERS SET name = ?, surname = ?, login = ?, email = ?, tel = ? WHERE login='"
+				        + user.getLogin() + "'";
+	
+		try (Connection connection = ConnectionPool.getPool().getConnection();
+			PreparedStatement statement = connection.prepareStatement(query))
+		{
 			statement.setString(1, user.getName());
 			statement.setString(2, user.getSurname());
 			statement.setString(3, user.getLogin());
@@ -63,32 +55,27 @@ public class MySqlUserDAO implements UserDAO {
 			statement.executeUpdate();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		} finally {
-			ConnectionPool.getPool().closeDbResources(connection, statement, set);
 		}
 	}
 
 	@Override
 	public void storeUsers(List<User> users) {
+		
+		String query = "CREATE TABLE IF NOT EXISTS `users` ("
+			      + "`id`             INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,"
+			      + "`name`   	    VARCHAR(255) DEFAULT \"\"," + "`surname`     	VARCHAR(255) DEFAULT \"\","
+			      + "`login`      	VARCHAR(255) DEFAULT \"\"," + "`email`       	VARCHAR(255) DEFAULT \"\","
+		          + "`tel`	        VARCHAR(255) DEFAULT \"\");";
+		
 		dropTableIfExists();
-		Connection connection = null;
-		PreparedStatement statement = null;
 
-		try {
-			connection = ConnectionPool.getPool().getConnection();
-
-			statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS `users` ("
-					+ "`id`             INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,"
-					+ "`name`   	    VARCHAR(255) DEFAULT \"\"," + "`surname`     	VARCHAR(255) DEFAULT \"\","
-					+ "`login`      	VARCHAR(255) DEFAULT \"\"," + "`email`       	VARCHAR(255) DEFAULT \"\","
-					+ "`tel`	        VARCHAR(255) DEFAULT \"\");");
-
+		try (Connection connection = ConnectionPool.getPool().getConnection();
+        	 PreparedStatement statement = connection.prepareStatement(query))
+		{
 			statement.executeUpdate();
 
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		} finally {
-			ConnectionPool.getPool().closeDbResources(connection, statement);
 		}
 
 		for (User user : users) {
@@ -98,36 +85,29 @@ public class MySqlUserDAO implements UserDAO {
 	}
 
 	private void dropTableIfExists() {
-		Connection connection = null;
-		PreparedStatement statement = null;
 
-		try {
-			connection = ConnectionPool.getPool().getConnection();
-
-			statement = connection.prepareStatement("DROP TABLE IF EXISTS `users`;");
+		String query = "DROP TABLE IF EXISTS `users`;";
+		
+		try (Connection connection = ConnectionPool.getPool().getConnection();
+ 			 PreparedStatement statement = connection.prepareStatement(query)) {
 
 			statement.executeUpdate();
 
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		} finally {
-			ConnectionPool.getPool().closeDbResources(connection, statement);
 		}
-
 	}
 
 	@Override
 	public List<User> loadAllUsers(String sort) {
-		Connection connection = null;
-		PreparedStatement statement = null;
-		ResultSet set = null;
 
+		String query = "SELECT * from users ORDER BY " + sort + " ";
+		
 		List<User> result = new ArrayList<User>();
 
-		try {
-			connection = ConnectionPool.getPool().getConnection();
-			statement = connection.prepareStatement("SELECT * from users ORDER BY " + sort + " ");
-			set = statement.executeQuery();
+		try (Connection connection = ConnectionPool.getPool().getConnection();
+			 PreparedStatement statement = connection.prepareStatement(query);
+			 ResultSet set = statement.executeQuery()) {
 
 			while (set.next()) {
 				User entity = ResultSetConverter.createUserEntity(set);
@@ -135,8 +115,6 @@ public class MySqlUserDAO implements UserDAO {
 			}
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		} finally {
-			ConnectionPool.getPool().closeDbResources(connection, statement, set);
 		}
 
 		return result;
--- src/babroval/testtask/dao/mysql/db/ConnectionPool.java
+++ src/babroval/testtask/dao/mysql/db/ConnectionPool.java
@@ -2,8 +2,6 @@ package babroval.testtask.dao.mysql.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectionPool {
@@ -40,44 +38,4 @@ public class ConnectionPool {
 		}
 	}
 
-	public void closeDbResources(Connection connection) {
-		closeDbResources(connection, null);
-	}
-
-	public void closeDbResources(Connection connection, PreparedStatement statement) {
-		closeDbResources(connection, statement, null);
-	}
-
-	public void closeDbResources(Connection connection, PreparedStatement statement, ResultSet resultSet) {
-		closeResultSet(resultSet);
-		closeStatement(statement);
-		closeConnection(connection);
-	}
-
-	private void closeConnection(Connection connection) {
-		if (connection != null) {
-			try {
-				connection.close();
-			} catch (SQLException e) {
-			}
-		}
-	}
-
-	private void closeStatement(PreparedStatement statement) {
-		if (statement != null) {
-			try {
-				statement.close();
-			} catch (SQLException e) {
-			}
-		}
-	}
-
-	private void closeResultSet(ResultSet resultSet) {
-		if (resultSet != null) {
-			try {
-				resultSet.close();
-			} catch (SQLException e) {
-			}
-		}
-	}
 }
