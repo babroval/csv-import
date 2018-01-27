0fcccdae369547f5a20417678b47e506577ab44b
babroval <maksimbabrou@gmail.com>
2018-01-27 22:04:08 +0100

    Add an exception handler

---
+++ src/babroval/testtask/controllers/ExceptionController.java
@@ -0,0 +1,36 @@
+package babroval.testtask.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ExceptionController")
+public class ExceptionController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final String VIEW_EXCEPTION = "WEB-INF/pages/exception.jsp";
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+
+		request.setAttribute("error", throwable.getMessage());
+
+		RequestDispatcher rd = request.getRequestDispatcher(VIEW_EXCEPTION);
+		rd.forward(request, response);
+		return;
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
