2d41ce23d85f920f72f7382937979ac882c7d6b6
babroval <maksimbabrou@gmail.com>
2018-01-31 23:08:37 +0100

    Edit DaoFactory for compactness

--- src/babroval/testtask/dao/factory/DAOFactory.java
+++ src/babroval/testtask/dao/factory/DAOFactory.java
@@ -6,21 +6,17 @@ public abstract class DAOFactory {
 
 	public abstract UserDAO getUserDAO();
 
-	public static DAOFactory getFactory() {
-		return getFactory(StoradgeTypes.MySql);
-	}
-
-	
 	public static DAOFactory getFactory(StoradgeTypes type) {
-		
+
 		switch (type) {
-			case MySql: {
+			case MySql:
 				return new MySqlDAOFactory();
-			}
-			case Csv: {
+				
+			case Csv:
 				return new CsvDAOFactory();
-			}
+				
+			default:
+				throw new RuntimeException("Couldn't create DAOFactory: " + type);
 		}
-		throw new RuntimeException("Couldn't create DAOFactory: " + type);
 	}
 }
